2a61c1d1a2da4bc558e9840ad155a736f773a7cb
Rick Farina <rickrfarina@gmail.com>
2017-10-24 16:30:04 -0500

    initial components and app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping/shopping-list-edit/shopping-list-edit.component';
+import { RecipiesComponent } from './recipe/recipies/recipies.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeListItemComponent } from './recipe/recipe-list-item/recipe-list-item.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeListItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
+    RecipiesComponent
   ],
   imports: [
     BrowserModule,
